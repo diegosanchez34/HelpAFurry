ffd3b0f91dc17a24616d3eb3aa75fca84c880922
BurritoLopr <shadowcb@gmail.com>
2023-04-18 14:16:05 -0400

    javascript

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function validarusuario(){
+    var camponombre1=document.getElementById('primerNombre').value;
+    var camponombre2=document.getElementById("segundoNombre").value;
+    var camponombre2=document.getElementById("primerApellido").value;
+    var camponombre2=document.getElementById("segundoApellido").value;
+    var campocorreo=document.getElementById('correo').value;
+    if(camponombre1==""||camponombre2==""||primerApellido==""||segundoApellido==""||campocorreo==""){
+        alert('Ta vacio');
+    }else{
+        alert("Ta ien");
+    }
+  }
+  function largo(){
+    var minlargo=50;
+    var campocomentario=document.getElementById('comentarios').val().length;
+    if(campocomentario<minlargo){
+        var campopelado = document.getElementById("errorlength");
+        campopelado.parentNode.innerHTML = "<p>te pasaste</p>";
+    }
+}
