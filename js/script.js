9df360e0e267af567089f858e2af0bd943f0ecc3
Eyleen Pino <ey.pino@duocuc.cl>
2023-04-23 16:22:55 -0400

    cambios finales a menu y js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
 function validarusuario(){
+    var camponombre1=$("#primerNombre").val();
+    var camponombre2=$("#segundoNombre").val();
+    var camponombre2=$("#primerApellido").val();
+    var camponombre2=$("#segundoApellido").val();
+    var campocorreo=$("#correo").val();
+    if(camponombre1==""||camponombre2==""||primerApellido==""||segundoApellido==""||campocorreo==""){
+        alert('Por favor rellene los campos');
+    }
+    if (largo()==false){
+    }
+    else{
+        alert("Su formulario ha sido enviado");
+    }
+    
+}
+
+function largo(){
+
+    var minlargo=50;
+    var campocomentario=$("#comentarios").val().length;
+    if(campocomentario<minlargo){
+        var campopelado = $("#errorlength");
+        alert("No ha completado por qué quiere pertenecer a la ONG");
+        return false;
+    }
+    else return true;
+}
+
+function limpiar(){
+    $(document).ready(function() {
+        $("input[type='reset']").click(function() {
+          $(".formulario")[0].reset();
+        });
+      });
+}
+
+ /*
+ 
+function validarusuario(){
     var camponombre1=document.getElementById('primerNombre').value;
     var camponombre2=document.getElementById("segundoNombre").value;
     var camponombre2=document.getElementById("primerApellido").value;
@@ -7,7 +46,7 @@ function validarusuario(){
     if(camponombre1==""||camponombre2==""||primerApellido==""||segundoApellido==""||campocorreo==""){
         alert('Por favor rellene los campos');
     }else{
-        alert("Está bien");
+        alert("Su formulario ha sido enviado");
     }
   }
   function largo(){
@@ -18,3 +57,12 @@ function validarusuario(){
         campopelado.parentNode.innerHTML = "<p>te pasaste</p>";
     }
 }
+
+ function largo(){
+    var minlargo=50;
+    var campocomentario=document.getElementById('comentarios').val().length;
+    if(campocomentario<minlargo){
+        var campopelado = document.getElementById("errorlength");
+        campopelado.parentNode.innerHTML = "<p>te pasaste</p>";
+    }
+}*/
